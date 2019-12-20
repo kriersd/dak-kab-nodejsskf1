987f156e3763d780a4f26e60f6f671c2aae6c0e8
David Krier <dkflyboy@gmail.com>
2019-12-20 13:57:23 -0600

    testing

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', {msg: "Hello Express! - Testing! 2"});
+    res.render('index', {msg: "Hello Express! - Testing! 3"});
 });
 
 module.exports = router;
