d630ffd789c8043baff2ea60967e0f2c38de1382
David Krier <dkflyboy@gmail.com>
2019-12-19 18:23:08 -0600

    asdfasdf

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', {msg: "Hello Express! - Testing!"});
+    res.render('index', {msg: "Hello Express! - Testing! 2"});
 });
 
 module.exports = router;
